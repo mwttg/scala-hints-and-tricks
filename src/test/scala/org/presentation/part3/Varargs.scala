f3b2df1e2adc648e3417ac9c7da9e75df4eb61a6
Marco Wittig <marco.wittig@jambit.com>
2019-02-20 09:36:32 +0100

    Improve description for Vararg example

--- src/test/scala/org/presentation/part3/Varargs.scala
+++ src/test/scala/org/presentation/part3/Varargs.scala
@@ -6,7 +6,7 @@ class Varargs extends WordSpec with Matchers {
     "be used like" in {
       method("Hello", "World!", "It's", "me", "Mario!")
     }
-    "what happen if you want to use a List?" in {
+    "what happen if you want to use a List as method parameter?" in {
       val input = Array("Hello", "World!", "It's", "me", "Mario!")
       // method(input)
       // This creates a compile error, but see method2
